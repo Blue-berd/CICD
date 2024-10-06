76fd1dd7bd64bfc44cb53a73d19b443a96edad4f
jay sardar <sardarjay4@gmail.com>
2024-10-07 01:33:52 +0530

    may it be success-16

--- src/app.test.js
+++ src/app.test.js
@@ -3,8 +3,18 @@ import { expect } from "chai";
 import app from "./app.js"; 
 
 describe("GET /", () => {
+  let server;
+
+  before((done) => {
+    server = app.listen(3001, done);
+  });
+
+  after((done) => {
+    server.close(done);
+  });
+
   it("should return Hello World", async () => {
-    const res = await request(app).get("/");
+    const res = await request(server).get("/");
     expect(res.text).to.equal("Hello World!");
     expect(res.statusCode).to.equal(200);
   });
