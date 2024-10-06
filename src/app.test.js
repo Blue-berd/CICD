7a2fa546f660b355e98a5907b19822872642601f
jay sardar <sardarjay4@gmail.com>
2024-10-07 02:21:10 +0530

    may it be success-33

--- src/app.test.js
+++ src/app.test.js
@@ -8,20 +8,6 @@ dotenv.config();
 let server;
 
 describe("GET /", () => {
-  // Start the server before tests
-  before((done) => {
-    server = app.listen(process.env.PORT, done);
-  });
-
-  // Close the server after tests
-  after((done) => {
-    if (server) {
-      server.close(done);
-    } else {
-      done();
-    }
-  });
-
   it("should return Hello World", async () => {
     const res = await request(server).get("/");
     expect(res.text).to.equal("Hello World");
