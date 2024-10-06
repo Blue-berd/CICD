af4341c1892132625a84cda62afc55f331d3eab4
jay sardar <sardarjay4@gmail.com>
2024-10-07 02:25:44 +0530

    may it be success-34

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import express from "express";
-import dotenv from 'dotenv'
-dotenv.config()
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
-});
+
 export default app;
--- src/app.test.js
+++ src/app.test.js
@@ -7,6 +7,20 @@ dotenv.config();
 
 let server;
 
+before((done) => {
+  server = app.listen(3001, () => {
+    console.log("Test server is running on port 3001");
+    done();
+  });
+});
+
+after((done) => {
+  server.close(() => {
+    console.log("Test server closed");
+    done();
+  });
+});
+
 describe("GET /", () => {
   it("should return Hello World", async () => {
     const res = await request(server).get("/");
